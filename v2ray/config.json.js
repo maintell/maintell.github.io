9c5e59531bfe0e579ba6961c255bfd858436aa04
蒋林国 <jianglinguo@eissoft.com>
2019-05-23 13:46:00 +0800

    软件中心增加

---
+++ v2ray/config.json.js
@@ -0,0 +1,9 @@
+{
+"version":"2.3.7",
+"md5":"8eb2acf2941bea79a10e56e1e973cf5c",
+"home_url":"Module_v2ray.asp",
+"title":"V2Ray",
+"description":"模块化的代理软件包",
+"changelog":"增加本地端口流量标记",
+"build_date":"2019-05-07_20:32:14"
+}
---
+++ wireguard/config.json.js
@@ -0,0 +1,9 @@
+{
+"version":"0.4",
+"md5":"509fda9127ebcc1fbd4aa110d8e67c19",
+"home_url":"Module_wireguard.asp",
+"title":"WireGuard",
+"description":"高效的次世代VPN",
+"changelog":"删除smartdns",
+"build_date":"2019-03-28_22:37:07"
+}
